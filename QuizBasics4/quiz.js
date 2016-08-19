02a9927e229e20495afc53134fb45aa7bc1bd548
Rene Gomez <renegomezjr@gmail.com>
2016-08-19 15:40:16 -0400

    quiz game.

---
+++ QuizBasics4/quiz.js
@@ -0,0 +1,7 @@
+var answer = prompt("What programming language is the name of a gem?");
+
+if(answer.toUpperCase() === "RUBY"){
+    document.write("<p>That's right!</p>");
+} else{
+  document.write("<p>Nope, that's wrong.</p>");  
+}
