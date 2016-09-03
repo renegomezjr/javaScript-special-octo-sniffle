ff3e61c4d73650d3ff68cd9c6a8488bb711c26f7
Rene Gomez <renegomezjr@gmail.com>
2016-09-03 12:28:04 -0400

    Added new section to complete quiz game.

---
+++ ConditionsGuessingGame6/quiz.js
@@ -0,0 +1,32 @@
+/*
+1. 5 quiz questions
+2. Print to screen how many were correct
+3. Give award for the number correct.
+*/
+
+var response =;
+var points = ;
+    
+
+prompt("What is my favorite color?")
+
+if{}
+prompt("What is my favorite color?")
+if{}
+prompt("What is my favorite color?")
+if{}
+prompt("What is my favorite color?")
+if{}
+prompt("What is my favorite color?")
+if{}
+
+if(5){
+    
+}else if(3 or 4){
+    
+}else if(1 or 2){
+    
+}else{
+    
+}
+    
