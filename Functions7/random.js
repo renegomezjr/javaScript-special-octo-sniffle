7abba0e903c837eec18b37ec9d236678f5c6800d
Rene Gomez <renegomezjr@gmail.com>
2016-09-28 15:31:17 -0400

    basics for random number and return statement

---
+++ Functions7/random.js
@@ -0,0 +1,14 @@
+//function alertRandom(){
+//    var random = Math.floor( Math.random() * 6) +1;
+//    alert(random);
+//}
+//
+//alertRandom();
+function getRandom(){
+    var random = Math.floor( Math.random() * 6) +1;
+    return random;
+}
+
+alert(getRandom());
+console.log(getRandom());
+document.write(getRandom());
