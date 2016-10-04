9132845d5890a4a3dd97632a89e03b4fa570a1cd
Rene Gomez <renegomezjr@gmail.com>
2016-10-04 16:18:00 -0400

    Adding function

--- JavaScript_Loops_Arrays_Objects/Section_1_ Loops/video06_solution/js/script.js
+++ JavaScript_Loops_Arrays_Objects/Section_1_ Loops/video06_solution/js/script.js
@@ -3,11 +3,13 @@ var red;
 var green;
 var blue;
 var rgbColor;
-
-for(var i = 0; i < 10; i++){
-    red = Math.floor(Math.random() * 256 );
-    green = Math.floor(Math.random() * 256 );
-    blue = Math.floor(Math.random() * 256 );
+function getRandom(){
+    return Math.floor(Math.random() * 256 );
+}
+for(var i = 0; i < 1000; i++){
+    red = getRandom();
+    green = getRandom();
+    blue = getRandom();
     rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
     html += '<div style="background-color:' + rgbColor + '"></div>';
 }
