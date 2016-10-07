8a31e5d133edd34906b297ed7f91c60125748e8d
Rene Gomez <renegomezjr@gmail.com>
2016-10-07 12:04:34 -0400

    For loops and arrays

---
+++ JavaScript_Loops_Arrays_Objects/Section_2_Arrays/video03/js/playlist.js
@@ -0,0 +1,7 @@
+var playList = [];
+playList.push('I Did It My Way');
+playList.push('Respect', 'Imagine');
+playList.push('Get Low');
+playList.unshift('Wonderwall', 'YMCA');
+
+printList(playList);
