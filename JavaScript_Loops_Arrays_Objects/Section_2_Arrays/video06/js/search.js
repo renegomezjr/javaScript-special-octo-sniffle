f793ef29ea01300d59bb1a61b39def2936aca728
Rene Gomez <renegomezjr@gmail.com>
2016-10-11 10:10:16 -0400

    Lesson 6 join method with arrays.

--- JavaScript_Loops_Arrays_Objects/Section_2_Arrays/video06/js/search.js
+++ JavaScript_Loops_Arrays_Objects/Section_2_Arrays/video06/js/search.js
@@ -5,3 +5,19 @@ function print(message) {
   document.write( '<p>' + message + '</p>');
 }
 
+while(true){
+    search = prompt("Search for a product in our store. Type 'list' to show all of the produce and 'quit' to exit.");
+    search = search.toLowerCase();
+    if(search === 'quit'){
+        break;
+    }else if(search === 'list'){
+        print(inStock.join(', '));
+    } else{
+        if(inStock.indexOf(search)>-1){
+            print('Yes, we have ' + search + ' in the store.');
+        }else{
+            print((search + ' is not in stock.'));
+        }
+    }
+}
+
