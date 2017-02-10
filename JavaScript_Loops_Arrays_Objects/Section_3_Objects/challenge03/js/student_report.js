a2467543ffdcd09426a158630e4b901ed1c59e2d
Rene Gomez <renegomezjr@gmail.com>
2017-02-10 14:55:26 -0500

    Complete challenge 3

--- JavaScript_Loops_Arrays_Objects/Section_3_Objects/challenge03/js/student_report.js
+++ JavaScript_Loops_Arrays_Objects/Section_3_Objects/challenge03/js/student_report.js
@@ -1,15 +1,26 @@
-var message = '';
-var student;
+
 
 function print(message) {
   var outputDiv = document.getElementById('output');
   outputDiv.innerHTML = message;
 }
 
+var choice = prompt('Which student would you like to find? Or enter "quit" to exit:');
+
+while(choice != 'quit'){ 
+var message = '';
+var student;
 for (var i = 0; i < students.length; i += 1) {
   student = students[i];
-  message += '<h2>Student: ' + student.name + '</h2>';
-  message += '<p>Track: ' + student.track + '</p>';
-  message += '<p>Points: ' + student.points + '</p>';
-  message += '<p>Achievements: ' + student.achievements + '</p>';
-}
+    if(choice === student.name){
+        message += '<h2>Student: ' + student.name + '</h2>';
+        message += '<p>Track: ' + student.track + '</p>';
+        message += '<p>Points: ' + student.points + '</p>';
+        message += '<p>Achievements: ' + student.achievements + '</p>';
+        print(message);
+        break;
+    }
+};
+
+    choice = prompt('Which student would you like to find? Or enter "quit" to exit:');
+};
